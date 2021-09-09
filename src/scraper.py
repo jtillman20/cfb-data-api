7d52d09ef032821c7d99a59d1ed0b4505168f64b
jtillman20 <jordanmtillman@gmail.com>
2021-09-09 19:30:58 -0400

    Implement method for parsing team and conference data

--- src/scraper.py
+++ src/scraper.py
@@ -1,8 +1,46 @@
+import re
+
+from bs4 import BeautifulSoup
 from requests import Session
 
 
 class SportsReferenceScraper(object):
     BASE_URL = 'https://sports-reference.com/cfb/years'
+    CONFERENCE_NAME_PATTERN = r'([A-Za-z0-9- ]+) \([A-Za-z]+\)'
+
+    # Conference names that are modified from how they appear on Sports
+    # Reference to how to store them in the database
+    CONFERENCES_NAMES = {
+        'ACC': 'Atlantic Coast',
+        'American': 'American Athletic',
+        'BIAA': 'Border',
+        'CUSA': 'Conference USA',
+        'Ind': 'Independent',
+        'MAC': 'Mid-American',
+        'MVC': 'Missouri Valley',
+        'MWC': 'Mountain West',
+        'PCC': 'Pacific Coast',
+        'SEC': 'Southeastern',
+        'SWAC': 'Southwestern Athletic',
+        'SWC': 'Southwest',
+        'WAC': 'Western Athletic'
+    }
+
+    # Team names that are modified from how they appear on Sports
+    # Reference to how to store them in the database
+    TEAMS_NAMES = {
+        'Bowling Green State': 'Bowling Green',
+        'California-Santa Barbara': 'UCSB',
+        'Louisiana': 'Louisiana-Lafayette',
+        'Massachusetts': 'UMass',
+        'Middle Tennessee State': 'Middle Tennessee',
+        'Nevada-Las Vegas': 'UNLV',
+        'North Carolina State': 'NC State',
+        'Ole Miss': 'Mississippi',
+        'Pitt': 'Pittsburgh',
+        'Texas Christian': 'TCU',
+        'Virginia Military Institute': 'VMI'
+    }
 
     def __init__(self):
         self.session = Session()
@@ -20,3 +58,34 @@ class SportsReferenceScraper(object):
         """
         url = f'{self.BASE_URL}/{path}'
         return self.session.get(url).content.decode('latin-1')
+
+    @classmethod
+    def parse_standings_html_data(cls, html_content: str) -> tuple:
+        """
+        Parse the HTML data to get each team and its associated
+        conference.
+
+        Args:
+            html_content (str): Web page HTML data
+
+        Returns:
+            tuple: Team and conference name
+        """
+        soup = BeautifulSoup(html_content, 'lxml')
+        rows = soup.find(id='standings').find('tbody').find_all('tr')
+
+        for row in rows:
+            # Header rows have a class attribute so skip them
+            if row.get('class') is not None:
+                continue
+
+            row_data = row.find_all('td')
+            team = row_data[0].a.text
+
+            result = re.findall(cls.CONFERENCE_NAME_PATTERN, row_data[1].text)
+            conference = result[0].strip() if result else row_data[1].text
+
+            team = cls.TEAMS_NAMES.get(team) or team
+            conference = cls.CONFERENCES_NAMES.get(conference) or conference
+
+            yield team, conference
