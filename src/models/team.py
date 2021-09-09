e46ea8ec965bf5f635edaa15b1ff021bdb431674
jtillman20 <jordanmtillman@gmail.com>
2021-09-08 21:08:56 -0400

    Create team class

---
+++ src/models/team.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Team(db.Model):
+    __tablename__ = 'team'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    nickname = db.Column(db.String(100), nullable=True)
