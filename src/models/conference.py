1ce33eac8ab4d4aa3ab5cabbbd3a90b22f9a0b76
jtillman20 <jordanmtillman@gmail.com>
2021-09-09 19:34:19 -0400

    Implement methods for getting conferences and a conference's teams

--- src/models/conference.py
+++ src/models/conference.py
@@ -8,6 +8,38 @@ class Conference(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
+    @classmethod
+    def get_conferences(cls, year: int) -> list['Conference']:
+        """
+        Get all FBS conferences for the given year.
+
+        Args:
+            year (int): Year to get conferences for
+
+        Returns:
+            list[Conference]: All conferences
+        """
+        return list(filter(
+            lambda conference: any(
+                year in membership.years for membership in conference.teams
+            ), cls.query.all()
+        ))
+
+    def get_teams(self, year: int) -> list[str]:
+        """
+        Get the teams that belong to the conference for the given year.
+
+        Args:
+            year (int): Year to get a conference's teams
+
+        Returns:
+            list[str]: Teams who belong to the conference
+        """
+        return list(map(
+            lambda membership: membership.team.name,
+            filter(lambda membership: year in membership.years, self.teams)
+        ))
+
 
 class ConferenceMembership(db.Model):
     __tablename__ = 'conference_membership'
