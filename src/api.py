1ac364ba6e0f8ef65e77a01143530179c92ff248
jtillman20 <jordanmtillman@gmail.com>
2021-09-09 23:15:09 -0400

    Add team and conference endpoints

--- src/api.py
+++ src/api.py
@@ -1,6 +1,10 @@
 from flask_restful import Api
 
 from app import app
+from routes import *
 
 api = Api(app)
 API_BASE = '/api'
+
+api.add_resource(ConferenceRoute, f'{API_BASE}/conferences')
+api.add_resource(TeamRoute, f'{API_BASE}/teams')
