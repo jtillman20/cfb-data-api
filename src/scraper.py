c21c853354b1ba8d586204edca69b47b6c62e8b7
jtillman20 <jordanmtillman@gmail.com>
2021-09-14 22:36:08 -0400

    Implement method to parse game data

--- src/scraper.py
+++ src/scraper.py
@@ -24,9 +24,9 @@ class SportsReferenceScraper(object):
         'WAC': 'Western Athletic'
     }
 
-    # Team names that are modified from how they appear on Sports
-    # Reference to how to store them in the database
-    TEAM_NAMES = {
+    # Team names from the standings page that are modified from how they
+    # appear on Sports Reference to how to store them in the database
+    STANDINGS_TEAM_NAMES = {
         'Bowling Green State': 'Bowling Green',
         'California-Santa Barbara': 'UCSB',
         'Louisiana': 'Louisiana-Lafayette',
@@ -37,6 +37,30 @@ class SportsReferenceScraper(object):
         'Ole Miss': 'Mississippi',
         'Pitt': 'Pittsburgh',
         'Texas Christian': 'TCU',
+        'Virginia Military Institute': 'VMI'
+    }
+
+    # Team names from the schedule page that are modified from how they
+    # appear on Sports Reference to how to store them in the database
+    SCHEDULE_TEAM_NAMES = {
+        'Alabama-Birmingham': 'UAB',
+        'California-Davis': 'UC Davis',
+        'California-Riverside': 'UC Riverside',
+        'California-Santa Barbara': 'UCSB',
+        'Bowling Green State': 'Bowling Green',
+        'Brigham Young': 'BYU',
+        'Central Florida': 'UCF',
+        'Louisiana': 'Louisiana-Lafayette',
+        'Louisiana State': 'LSU',
+        'Massachusetts': 'UMass',
+        'Middle Tennessee State': 'Middle Tennessee',
+        'Nevada-Las Vegas': 'UNLV',
+        'North Carolina State': 'NC State',
+        'Southern California': 'USC',
+        'Southern Methodist': 'SMU',
+        'Texas Christian': 'TCU',
+        'Texas-El Paso': 'UTEP',
+        'Texas-San Antonio': 'UTSA',
         'Virginia Military Institute': 'VMI'
     }
 
@@ -81,7 +105,75 @@ class SportsReferenceScraper(object):
             team = row.find(attrs={'data-stat': 'school_name'}).text
             conference = row.find(attrs={'data-stat': 'conf_abbr'}).a.text
 
-            team = cls.TEAM_NAMES.get(team) or team
+            team = cls.STANDINGS_TEAM_NAMES.get(team) or team
             conference = cls.CONFERENCE_NAMES.get(conference) or conference
 
             yield team, conference
+
+    @classmethod
+    def parse_schedule_html_data(cls, html_content: str) -> dict:
+        """
+        Parse the HTML data to get information for every game.
+
+        Args:
+            html_content (str): Web page HTML data
+
+        Returns:
+            dict: Game information
+        """
+        soup = BeautifulSoup(html_content, 'lxml')
+        rows = soup.find(id='schedule').find('tbody').find_all('tr')
+
+        for row in rows:
+            # Header rows have a thead class attribute so skip them
+            row_class = row.get('class')
+            if row_class is not None and 'thead' in row_class:
+                continue
+
+            week = int(row.find(attrs={'data-stat': 'week_number'}).text)
+            date = dateutil.parser.parse(row.find(
+                attrs={'data-stat': 'date_game'}).text)
+            year = date.year
+
+            winning_score = int(row.find(
+                attrs={'data-stat': 'winner_points'}).text or 0)
+            losing_score = int(row.find(
+                attrs={'data-stat': 'loser_points'}).text or 0)
+
+            # This happens if a game is cancelled or hasn't been played yet
+            if not winning_score and not losing_score and year >= 1996:
+                continue
+
+            tag = row.find(attrs={'data-stat': 'winner_school_name'})
+            winning_team = tag.a.text if tag.a else tag.text
+
+            tag = row.find(attrs={'data-stat': 'loser_school_name'})
+            losing_team = tag.a.text if tag.a else tag.text
+
+            location = row.find(attrs={'data-stat': 'game_location'}).text
+            neutral_site = location == 'N'
+
+            if location == '@':
+                home_team = losing_team
+                home_score = losing_score
+                away_team = winning_team
+                away_score = winning_score
+            else:
+                home_team = winning_team
+                home_score = winning_score
+                away_team = losing_team
+                away_score = losing_score
+
+            home_team = cls.SCHEDULE_TEAM_NAMES.get(home_team) or home_team
+            away_team = cls.SCHEDULE_TEAM_NAMES.get(away_team) or away_team
+
+            yield {
+                'year': year,
+                'week': week,
+                'date': date,
+                'neutral_site': neutral_site,
+                'home_team': home_team,
+                'home_score': home_score,
+                'away_team': away_team,
+                'away_score': away_score
+            }
