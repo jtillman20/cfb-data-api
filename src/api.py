812679cde8a9a1e44a91e80648c3b505a3632180
jtillman20 <jordanmtillman@gmail.com>
2021-09-29 15:43:44 -0400

    Add conference srs endpoint

--- src/api.py
+++ src/api.py
@@ -11,3 +11,4 @@ api.add_resource(TeamRoute, f'{API_BASE}/teams')
 
 api.add_resource(RecordRoute, f'{API_BASE}/records')
 api.add_resource(SRSRoute, f'{API_BASE}/srs_ratings')
+api.add_resource(ConferenceSRSRoute, f'{API_BASE}/conference_srs_ratings')
