8af5cd2f08e98313a516af0f97367fa708477283
jtillman20 <jordanmtillman@gmail.com>
2021-10-05 21:21:15 -0400

    Create rpi class

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from .conference import Conference, ConferenceMembership
 from .game import Game
 from .record import Record
+from .rpi import RPI
 from .srs import ConferenceSRS, SRS
 from .team import Team
