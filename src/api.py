5477b4a6cb607a3ac6cf5bea550c9619cdaf96e9
jtillman20 <jordanmtillman@gmail.com>
2021-10-05 21:29:02 -0400

    Add rpi endpoint

--- src/api.py
+++ src/api.py
@@ -10,5 +10,6 @@ api.add_resource(ConferenceRoute, f'{API_BASE}/conferences')
 api.add_resource(TeamRoute, f'{API_BASE}/teams')
 
 api.add_resource(RecordRoute, f'{API_BASE}/records')
+api.add_resource(RPIRoute, f'{API_BASE}/rpi_ratings')
 api.add_resource(SRSRoute, f'{API_BASE}/srs_ratings')
 api.add_resource(ConferenceSRSRoute, f'{API_BASE}/conference_srs_ratings')
