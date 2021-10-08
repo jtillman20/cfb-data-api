ffbee210a4c23bb468ba0c7a0e4025ebac6083db
jtillman20 <jordanmtillman@gmail.com>
2021-10-07 20:06:36 -0400

    Add ap poll ranking endpoint

--- src/api.py
+++ src/api.py
@@ -9,6 +9,8 @@ API_BASE = '/api'
 api.add_resource(ConferenceRoute, f'{API_BASE}/conferences')
 api.add_resource(TeamRoute, f'{API_BASE}/teams')
 
+api.add_resource(APPollRankingRoute, f'{API_BASE}/ap_poll_rankings')
+
 api.add_resource(RecordRoute, f'{API_BASE}/records')
 api.add_resource(RPIRoute, f'{API_BASE}/rpi_ratings')
 api.add_resource(SRSRoute, f'{API_BASE}/srs_ratings')
