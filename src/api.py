d10d39fb26a67ca7c5f98cf804f9213cb7cbf138
jtillman20 <jordanmtillman@gmail.com>
2021-10-14 23:34:19 -0400

    Add rushing endpoint

--- src/api.py
+++ src/api.py
@@ -18,4 +18,5 @@ api.add_resource(SRSRoute, f'{API_BASE}/srs_ratings')
 api.add_resource(ConferenceSRSRoute, f'{API_BASE}/conference_srs_ratings')
 
 api.add_resource(PassingRoute, f'{API_BASE}/passing/<string:side_of_ball>')
+api.add_resource(RushingRoute, f'{API_BASE}/rushing/<string:side_of_ball>')
 api.add_resource(ScoringRoute, f'{API_BASE}/scoring/<string:side_of_ball>')
