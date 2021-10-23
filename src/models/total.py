6cb58c1883c6b2352f1a81abc8527c85b742e4c7
jtillman20 <jordanmtillman@gmail.com>
2021-10-22 20:48:40 -0400

    Implement methods for getting total data

--- src/models/total.py
+++ src/models/total.py
@@ -86,6 +86,49 @@ class Total(db.Model):
         return self
 
     @classmethod
+    def get_total(cls, side_of_ball: str, start_year: int, end_year: int = None,
+                  team: str = None) -> Union['Total', list['Total']]:
+        """
+        Get total offense or defense for qualifying teams for the
+        given years. If team is provided, only get total data for
+        that team.
+
+        Args:
+            side_of_ball (str): Offense or defense
+            start_year (int): Year to start getting total data
+            end_year (int): Year to stop getting total data
+            team (str): Team for which to get total data
+
+        Returns:
+            Union[Total, list[Total]]: Total offense or defense
+                for all teams or only for one team
+        """
+        if end_year is None:
+            end_year = start_year
+
+        qualifying_teams = Team.get_qualifying_teams(
+            start_year=start_year, end_year=end_year)
+
+        query = cls.query.join(Team).filter(
+            cls.side_of_ball == side_of_ball,
+            cls.year >= start_year,
+            cls.year <= end_year
+        )
+
+        if team is not None:
+            total = query.filter_by(name=team).all()
+            return sum(total[1:], total[0])
+
+        total = {}
+        for team_name in qualifying_teams:
+            team_total = query.filter_by(name=team_name).all()
+
+            if team_total:
+                total[team_name] = sum(team_total[1:], team_total[0])
+
+        return [total[team] for team in sorted(total.keys())]
+
+    @classmethod
     def add_total(cls, start_year: int, end_year: int) -> None:
         """
         Get total offense and defense stats for all teams for the
