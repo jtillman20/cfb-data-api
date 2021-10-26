e3d424b1b07ffe313b4a131325650eb010c32514
jtillman20 <jordanmtillman@gmail.com>
2021-10-26 17:55:01 -0400

    Change how to get rushing data using game stats and add relative data

--- src/models/rushing.py
+++ src/models/rushing.py
@@ -1,8 +1,7 @@
-from operator import attrgetter
 from typing import Union
 
 from app import db
-from scraper import CFBStatsScraper
+from .game import Game
 from .team import Team
 
 
@@ -16,6 +15,10 @@ class Rushing(db.Model):
     attempts = db.Column(db.Integer, nullable=False)
     yards = db.Column(db.Integer, nullable=False)
     tds = db.Column(db.Integer, nullable=False)
+    opponents_games = db.Column(db.Integer, nullable=False)
+    opponents_attempts = db.Column(db.Integer, nullable=False)
+    opponents_yards = db.Column(db.Integer, nullable=False)
+    opponents_tds = db.Column(db.Integer, nullable=False)
 
     @property
     def attempts_per_game(self) -> float:
@@ -41,6 +44,31 @@ class Rushing(db.Model):
             return self.tds / self.attempts * 100
         return 0.0
 
+    @property
+    def opponents_yards_per_game(self) -> float:
+        if self.opponents_games:
+            return self.opponents_yards / self.opponents_games
+        return 0.0
+
+    @property
+    def opponents_yards_per_attempt(self) -> float:
+        if self.opponents_attempts:
+            return self.opponents_yards / self.opponents_attempts
+        return 0.0
+
+    @property
+    def relative_yards_per_game(self) -> float:
+        if self.opponents_yards_per_game:
+            return (self.yards_per_game / self.opponents_yards_per_game) * 100
+        return 0.0
+
+    @property
+    def relative_yards_per_attempt(self) -> float:
+        if self.opponents_yards_per_attempt:
+            return (self.yards_per_attempt / self.opponents_yards_per_attempt) \
+                   * 100
+        return 0.0
+
     def __add__(self, other: 'Rushing') -> 'Rushing':
         """
         Add two Rushing objects to combine multiple years of data.
@@ -55,6 +83,10 @@ class Rushing(db.Model):
         self.attempts += other.attempts
         self.yards += other.yards
         self.tds += other.tds
+        self.opponents_games += other.opponents_games
+        self.opponents_attempts += other.opponents_attempts
+        self.opponents_yards += other.opponents_yards
+        self.opponents_tds += other.opponents_tds
 
         return self
 
@@ -103,18 +135,27 @@ class Rushing(db.Model):
         return [rushing[team] for team in sorted(rushing.keys())]
 
     @classmethod
-    def add_rushing(cls, start_year: int, end_year: int) -> None:
+    def add_rushing(cls, start_year: int = None, end_year: int = None) -> None:
         """
         Get rushing offense and defense stats for all teams for the
         given years and add them to the database.
 
         Args:
-            start_year (int): Year to start getting rushing stats
-            end_year (int): Year to stop getting rushing stats
+            start_year (int): Year to start adding rushing stats
+            end_year (int): Year to stop adding rushing stats
         """
-        for year in range(start_year, end_year + 1):
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            years = [year.year for year in query]
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
             print(f'Adding rushing stats for {year}')
             cls.add_rushing_for_one_year(year=year)
+            cls.add_opponent_rushing(year=year)
 
     @classmethod
     def add_rushing_for_one_year(cls, year: int) -> None:
@@ -125,30 +166,97 @@ class Rushing(db.Model):
         Args:
             year (int): Year to get rushing stats
         """
-        scraper = CFBStatsScraper(year=year)
+        teams = Team.get_teams(year=year)
 
-        for side_of_ball in ['offense', 'defense']:
-            rushing = []
+        for team in teams:
+            games = Game.get_games(year=year, team=team.name)
+            game_stats = [game.stats[0] for game in games]
 
-            html_content = scraper.get_html_data(
-                side_of_ball=side_of_ball, category='01')
-            rushing_data = scraper.parse_html_data(
-                html_content=html_content)
+            for side_of_ball in ['offense', 'defense']:
+                attempts = 0
+                yards = 0
+                tds = 0
 
-            for item in rushing_data:
-                team = Team.query.filter_by(name=item[1]).first()
-                rushing.append(cls(
+                for stats in game_stats:
+                    home_team = stats.game.home_team
+
+                    if side_of_ball == 'offense':
+                        side = 'home' if home_team == team.name else 'away'
+                    else:
+                        side = 'away' if home_team == team.name else 'home'
+
+                    attempts += getattr(stats, f'{side}_rushing_attempts')
+                    yards += getattr(stats, f'{side}_rushing_yards')
+                    tds += getattr(stats, f'{side}_rushing_tds')
+
+                db.session.add(cls(
                     team_id=team.id,
                     year=year,
                     side_of_ball=side_of_ball,
-                    games=item[2],
-                    attempts=item[3],
-                    yards=item[4],
-                    tds=item[6]
+                    games=len(games),
+                    attempts=attempts,
+                    yards=yards,
+                    tds=tds,
+                    opponents_games=0,
+                    opponents_attempts=0,
+                    opponents_yards=0,
+                    opponents_tds=0
                 ))
 
-            for team_rushing in sorted(rushing, key=attrgetter('team_id')):
-                db.session.add(team_rushing)
+        db.session.commit()
+
+    @classmethod
+    def add_opponent_rushing(cls, year: int) -> None:
+        """
+        Get rushing offense and defense for all team's opponents
+        and add them to the database.
+
+        Args:
+            year (int): Year to get rushing stats
+        """
+        rushing = cls.query.filter_by(year=year).all()
+
+        for team_rushing in rushing:
+            team = team_rushing.team.name
+            schedule = Game.get_games(year=year, team=team)
+
+            for game in schedule:
+                game_stats = game.stats[0]
+
+                if team == game.away_team:
+                    opponent_name = game.home_team
+                    attempts = game_stats.away_rushing_attempts
+                    yards = game_stats.away_rushing_yards
+                    tds = game_stats.away_rushing_tds
+                else:
+                    opponent_name = game.away_team
+                    attempts = game_stats.home_rushing_attempts
+                    yards = game_stats.home_rushing_yards
+                    tds = game_stats.home_rushing_tds
+
+                opponent_query = cls.query.filter_by(year=year).join(
+                    Team).filter_by(name=opponent_name)
+
+                if opponent_query.first() is not None:
+                    side_of_ball = team_rushing.side_of_ball
+                    opposite_side_of_ball = 'defense' \
+                        if side_of_ball == 'offense' else 'offense'
+
+                    opponent_stats = cls.query.filter_by(
+                        year=year, side_of_ball=opposite_side_of_ball).join(
+                        Team).filter_by(name=opponent_name).first()
+
+                    opponent_games = opponent_stats.games
+                    team_rushing.opponents_games += opponent_games - 1
+
+                    opponent_attempts = opponent_stats.attempts - attempts
+                    team_rushing.opponents_attempts += opponent_attempts
+
+                    opponent_yards = opponent_stats.yards - yards
+                    team_rushing.opponents_yards += opponent_yards
+
+                    opponent_tds = opponent_stats.tds - tds
+                    team_rushing.opponents_tds += opponent_tds
 
         db.session.commit()
 
@@ -165,7 +273,10 @@ class Rushing(db.Model):
             'yards_per_attempt': round(self.yards_per_attempt, 2),
             'yards_per_game': round(self.yards_per_game, 1),
             'tds': self.tds,
-            'td_pct': round(self.td_pct, 2)
+            'td_pct': round(self.td_pct, 2),
+            'relative_yards_per_attempt': round(
+                self.relative_yards_per_attempt, 1),
+            'relative_yards_per_game': round(self.relative_yards_per_game, 1)
         }
 
         if hasattr(self, 'rank'):
