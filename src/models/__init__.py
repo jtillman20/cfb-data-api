8e14258c63edc72666fe1c4eb451cd2246c2ca4b
jtillman20 <jordanmtillman@gmail.com>
2021-11-19 21:34:29 -0500

    Create passing plays class

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,11 +2,11 @@ from .ap_poll import APPoll, APPollRanking
 from .conference import Conference, ConferenceMembership
 from .first_downs import FirstDowns
 from .game import Game, GameStats
-from .passing import Passing
+from .passing import Passing, PassingPlays
 from .record import Record
 from .rpi import RPI
 from .rushing import Rushing
 from .scoring import Scoring
 from .srs import ConferenceSRS, SRS
 from .team import Team
-from .total import Total
+from .total import Total, ScrimmagePlays
