1c584d2c18ee1c97bc4904fcda8125b3f9ed920f
jtillman20 <jordanmtillman@gmail.com>
2021-11-20 20:58:07 -0500

    Implement methods for adding rushing plays to database

--- src/models/rushing.py
+++ src/models/rushing.py
@@ -1,6 +1,8 @@
+from operator import attrgetter
 from typing import Union
 
 from app import db
+from scraper import CFBStatsScraper
 from .first_downs import FirstDowns
 from .game import Game
 from .team import Team
@@ -387,6 +389,77 @@ class RushingPlays(db.Model):
         self.plays += other.plays
 
         return self
+
+    @classmethod
+    def add_rushing_plays(cls, start_year: int = None,
+                          end_year: int = None) -> None:
+        """
+        Get rushing plays and opponent rushing plays for all teams
+        for the given years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding rushing play stats
+            end_year (int): Year to stop adding rushing play stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            end_year = max([year.year for year in query])
+            years = range(2010, end_year + 1)
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding rushing play stats for {year}')
+            cls.add_rushing_plays_for_one_year(year=year)
+
+    @classmethod
+    def add_rushing_plays_for_one_year(cls, year: int) -> None:
+        """
+        Get rushing plays and opponent rushing plays for all teams
+        for one year and add them to the database.
+
+        Args:
+            year (int): Year to add rushing play stats
+        """
+        scraper = CFBStatsScraper(year=year)
+
+        for side_of_ball in ['offense', 'defense']:
+            rushing_plays = []
+
+            html_content = scraper.get_html_data(
+                side_of_ball=side_of_ball, category='31')
+            rushing_play_data = scraper.parse_html_data(
+                html_content=html_content)
+
+            for item in rushing_play_data:
+                team = Team.query.filter_by(name=item[1]).first()
+                rushing = Rushing.get_rushing(
+                    side_of_ball=side_of_ball, start_year=year, team=team.name)
+
+                rushing_plays.append(cls(
+                    team_id=team.id,
+                    year=year,
+                    side_of_ball=side_of_ball,
+                    games=item[2],
+                    ten=item[3],
+                    twenty=item[4],
+                    thirty=item[5],
+                    forty=item[6],
+                    fifty=item[7],
+                    sixty=item[8],
+                    seventy=item[9],
+                    eighty=item[10],
+                    ninety=item[11],
+                    plays=rushing.attempts
+                ))
+
+            for team_rushing_plays in sorted(
+                    rushing_plays, key=attrgetter('team_id')):
+                db.session.add(team_rushing_plays)
+
+        db.session.commit()
 
     def __getstate__(self) -> dict:
         data = {
