a07647bcdf5feeb9e4fd53108ffd3f7ce1adad6f
jtillman20 <jordanmtillman@gmail.com>
2021-11-20 21:02:22 -0500

    Add endpoint for rushing plays

--- src/api.py
+++ src/api.py
@@ -21,6 +21,8 @@ api.add_resource(PassingRoute, f'{API_BASE}/passing/<string:side_of_ball>')
 api.add_resource(
     PassingPlaysRoute, f'{API_BASE}/passing_plays/<string:side_of_ball>')
 api.add_resource(RushingRoute, f'{API_BASE}/rushing/<string:side_of_ball>')
+api.add_resource(
+    RushingPlaysRoute, f'{API_BASE}/rushing_plays/<string:side_of_ball>')
 api.add_resource(ScoringRoute, f'{API_BASE}/scoring/<string:side_of_ball>')
 api.add_resource(
     ScrimmagePlaysRoute, f'{API_BASE}/scrimmage_plays/<string:side_of_ball>')
