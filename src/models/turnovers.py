4a470a9c02aba92b6b293d11e89c1d766208bf3e
jtillman20 <jordanmtillman@gmail.com>
2022-01-26 18:52:47 -0500

    Implement methods for adding turnovers to database

--- src/models/turnovers.py
+++ src/models/turnovers.py
@@ -1,4 +1,6 @@
 from app import db
+from .game import Game
+from .team import Team
 
 
 class Turnovers(db.Model):
@@ -41,6 +43,77 @@ class Turnovers(db.Model):
         if self.games:
             return self.margin / self.games
         return 0.0
+
+    @classmethod
+    def add_turnovers(cls, start_year: int = None,
+                      end_year: int = None) -> None:
+        """
+        Get turnovers and opponent turnovers for all teams for the
+        given years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding penalty stats
+            end_year (int): Year to stop adding penalty stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            end_year = max([year.year for year in query])
+            years = range(2010, end_year + 1)
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding turnover stats for {year}')
+            cls.add_turnovers_for_one_year(year=year)
+
+    @classmethod
+    def add_turnovers_for_one_year(cls, year: int) -> None:
+        """
+        Get turnovers and opponent turnovers for all teams for one year
+        and add them to the database.
+
+        Args:
+            year (int): Year to get penalty stats
+        """
+        teams = Team.get_teams(year=year)
+
+        for team in teams:
+            games = Game.get_games(year=year, team=team.name)
+            game_stats = [game.stats[0] for game in games]
+
+            ints = 0
+            fumbles = 0
+            opponent_ints = 0
+            opponent_fumbles = 0
+
+            for stats in game_stats:
+                home_team = stats.game.home_team
+
+                if home_team == team.name:
+                    side = 'home'
+                    opp_side = 'away'
+                else:
+                    side = 'away'
+                    opp_side = 'home'
+
+                ints += getattr(stats, f'{side}_ints')
+                fumbles += getattr(stats, f'{side}_fumbles')
+                opponent_ints += getattr(stats, f'{opp_side}_ints')
+                opponent_fumbles += getattr(stats, f'{opp_side}_fumbles')
+
+            db.session.add(cls(
+                team_id=team.id,
+                year=year,
+                games=len(games),
+                ints=ints,
+                fumbles=fumbles,
+                opponent_ints=opponent_ints,
+                opponent_fumbles=opponent_fumbles
+            ))
+
+        db.session.commit()
 
     def __add__(self, other: 'Turnovers') -> 'Turnovers':
         """
