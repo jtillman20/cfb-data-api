bcbf171cd23fcdcc9505005fc231846dd84792e0
jtillman20 <jordanmtillman@gmail.com>
2022-02-14 20:45:37 -0500

    Implement methods for adding third down data to database

--- src/models/conversions.py
+++ src/models/conversions.py
@@ -1,4 +1,10 @@
+from operator import attrgetter
+
 from app import db
+from scraper import CFBStatsScraper
+from .game import Game
+from .team import Team
+from .total import Total
 
 
 class ThirdDowns(db.Model):
@@ -23,6 +29,69 @@ class ThirdDowns(db.Model):
         if self.plays:
             return self.attempts / self.plays * 100
         return 0.0
+
+    @classmethod
+    def add_third_downs(cls, start_year: int = None,
+                        end_year: int = None) -> None:
+        """
+        Get third down offense and defense stats for all teams for the
+        given years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding third down stats
+            end_year (int): Year to stop adding third down stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            years = [year.year for year in query]
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding third down stats for {year}')
+            cls.add_third_downs_for_one_year(year=year)
+
+    @classmethod
+    def add_third_downs_for_one_year(cls, year: int) -> None:
+        """
+        Get third down offense and defense stats for all teams
+        for one year and add them to the database.
+
+        Args:
+            year (int): Year to add third down stats
+        """
+        scraper = CFBStatsScraper(year=year)
+
+        for side_of_ball in ['offense', 'defense']:
+            third_downs = []
+
+            html_content = scraper.get_html_data(
+                side_of_ball=side_of_ball, category='25')
+            third_down_data = scraper.parse_html_data(
+                html_content=html_content)
+
+            for item in third_down_data:
+                team = Team.query.filter_by(name=item[1]).first()
+                total = Total.get_total(
+                    side_of_ball=side_of_ball, start_year=year, team=team.name)
+
+                third_downs.append(cls(
+                    team_id=team.id,
+                    year=year,
+                    side_of_ball=side_of_ball,
+                    games=item[2],
+                    attempts=item[3],
+                    conversions=item[4],
+                    plays=total.plays
+                ))
+
+            for team_third_downs in sorted(
+                    third_downs, key=attrgetter('team_id')):
+                db.session.add(team_third_downs)
+
+        db.session.commit()
 
     def __add__(self, other: 'ThirdDowns') -> 'ThirdDowns':
         """
