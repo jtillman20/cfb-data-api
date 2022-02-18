2c316e56b9f832afd2cb87dd9432e2eb0e0569e6
jtillman20 <jordanmtillman@gmail.com>
2022-02-17 20:32:34 -0500

    Implement methods for adding fourth down data to database

--- src/models/conversions.py
+++ src/models/conversions.py
@@ -31,6 +31,69 @@ class FourthDowns(db.Model):
             return self.attempts / self.plays * 100
         return 0.0
 
+    @classmethod
+    def add_fourth_downs(cls, start_year: int = None,
+                         end_year: int = None) -> None:
+        """
+        Get fourth down offense and defense stats for all teams for the
+        given years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding fourth down stats
+            end_year (int): Year to stop adding fourth down stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            years = [year.year for year in query]
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding fourth down stats for {year}')
+            cls.add_fourth_downs_for_one_year(year=year)
+
+    @classmethod
+    def add_fourth_downs_for_one_year(cls, year: int) -> None:
+        """
+        Get fourth down offense and defense stats for all teams
+        for one year and add them to the database.
+
+        Args:
+            year (int): Year to add fourth down stats
+        """
+        scraper = CFBStatsScraper(year=year)
+
+        for side_of_ball in ['offense', 'defense']:
+            fourth_downs = []
+
+            html_content = scraper.get_html_data(
+                side_of_ball=side_of_ball, category='26')
+            fourth_down_data = scraper.parse_html_data(
+                html_content=html_content)
+
+            for item in fourth_down_data:
+                team = Team.query.filter_by(name=item[1]).first()
+                total = Total.get_total(
+                    side_of_ball=side_of_ball, start_year=year, team=team.name)
+
+                fourth_downs.append(cls(
+                    team_id=team.id,
+                    year=year,
+                    side_of_ball=side_of_ball,
+                    games=item[2],
+                    attempts=item[3],
+                    conversions=item[4],
+                    plays=total.plays
+                ))
+
+            for team_fourth_downs in sorted(
+                    fourth_downs, key=attrgetter('team_id')):
+                db.session.add(team_fourth_downs)
+
+        db.session.commit()
+
     def __add__(self, other: 'FourthDowns') -> 'FourthDowns':
         """
         Add two FourthDowns objects to combine multiple years of data.
