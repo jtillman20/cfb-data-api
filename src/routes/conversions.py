0a8d6fbeed8338d04b135daa9a81868d9fec6a0c
jtillman20 <jordanmtillman@gmail.com>
2022-02-22 20:39:55 -0500

    Create red zone route with get method

--- src/routes/conversions.py
+++ src/routes/conversions.py
@@ -1,8 +1,9 @@
+from inspect import stack
 from typing import Union
 
 from flask_restful import Resource
 
-from models import FourthDowns, ThirdDowns
+from models import FourthDowns, RedZone, ThirdDowns
 from utils import (
     check_side_of_ball,
     flask_response,
@@ -52,6 +53,46 @@ class FourthDownsRoute(Resource):
 
         fourth_downs = sort(data=fourth_downs, attrs=attrs, reverses=reverses)
         return rank(data=fourth_downs, attr=sort_attr)
+
+
+class RedZoneRoute(Resource):
+    @flask_response
+    def get(self, side_of_ball: str) -> Union[RedZone, list[RedZone]]:
+        """
+        GET request to get red zone offense or defense for the given
+        years. If team is provided only get red zone data for that
+        team.
+
+        Args:
+            side_of_ball (str): Offense or defense
+
+        Returns:
+            Union[RedZone, list[RedZone]]: Red zone data for all
+                teams or only red zone data for one team
+        """
+        check_side_of_ball(value=side_of_ball)
+
+        sort_attr = get_optional_param(name='sort', default_value='score_pct')
+        secondary_attr, secondary_reverse = secondary_sort(
+            attr=sort_attr, side_of_ball=side_of_ball)
+        start_year, end_year = get_multiple_year_params()
+        team = get_optional_param(name='team')
+
+        red_zone = RedZone.get_red_zone(
+            side_of_ball=side_of_ball,
+            start_year=start_year,
+            end_year=end_year,
+            team=team
+        )
+
+        if isinstance(red_zone, RedZone):
+            return red_zone
+
+        attrs = [secondary_attr, sort_attr]
+        reverses = [secondary_reverse, side_of_ball == 'offense']
+
+        red_zone = sort(data=red_zone, attrs=attrs, reverses=reverses)
+        return rank(data=red_zone, attr=sort_attr)
 
 
 class ThirdDownsRoute(Resource):
@@ -105,23 +146,45 @@ def secondary_sort(attr: str, side_of_ball: str) -> tuple:
     Returns:
         tuple: Secondary sort attribute and sort order
     """
-    if attr in ['conversion_pct', 'play_pct']:
-        secondary_attr = 'attempts'
+    class_name = stack()[1][0].f_locals['self'].__class__.__name__
 
-    elif attr in ['conversions', 'attempts']:
-        secondary_attr = 'conversion_pct'
+    if class_name in ['FourthDownsRoute', 'ThirdDownsRoute']:
+        if attr in ['conversion_pct', 'play_pct']:
+            secondary_attr = 'attempts'
+
+        elif attr in ['conversions', 'attempts']:
+            secondary_attr = 'conversion_pct'
+
+        else:
+            secondary_attr = attr
+
+        if attr not in ASC_SORT_ATTRS:
+            reverse = side_of_ball == 'offense'
+        else:
+            reverse = side_of_ball == 'defense'
+
+        if secondary_attr not in ASC_SORT_ATTRS:
+            secondary_reverse = side_of_ball == 'offense'
+        else:
+            secondary_reverse = side_of_ball == 'defense'
+
+        return [secondary_attr, attr], [secondary_reverse, reverse]
 
     else:
-        secondary_attr = attr
+        if attr in ['score_pct', 'td_pct', 'field_goal_pct',
+                    'points_per_attempt']:
+            secondary_attr = 'attempts'
 
-    if attr not in ASC_SORT_ATTRS:
-        reverse = side_of_ball == 'offense'
-    else:
-        reverse = side_of_ball == 'defense'
+        elif attr in ['attempts', 'scores']:
+            secondary_attr = 'score_pct'
 
-    if secondary_attr not in ASC_SORT_ATTRS:
-        secondary_reverse = side_of_ball == 'offense'
-    else:
-        secondary_reverse = side_of_ball == 'defense'
+        elif attr == 'tds':
+            secondary_attr = 'td_pct'
 
-    return [secondary_attr, attr], [secondary_reverse, reverse]
+        elif attr == 'field_goals':
+            secondary_attr = 'field_goal_pct'
+
+        else:
+            secondary_attr = attr
+
+        return secondary_attr, side_of_ball == 'offense'
