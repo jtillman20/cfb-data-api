b6801b3fcaa546382ecf040b6721eb7f309dcebf
jtillman20 <jordanmtillman@gmail.com>
2022-03-10 20:25:19 -0500

    Implement method for getting tackles for loss

--- src/models/tackles_for_loss.py
+++ src/models/tackles_for_loss.py
@@ -1,4 +1,5 @@
 from operator import attrgetter
+from typing import Union
 
 from app import db
 from scraper import CFBStatsScraper
@@ -35,6 +36,52 @@ class TacklesForLoss(db.Model):
         if self.tackles_for_loss:
             return self.yards / self.tackles_for_loss
         return 0.0
+
+    @classmethod
+    def get_tackles_for_loss(cls, side_of_ball: str, start_year: int,
+                             end_year: int = None, team: str = None
+                             ) -> Union['TacklesForLoss',
+                                        list['TacklesForLoss']]:
+        """
+        Get tackles for loss or opponent tackles for loss for qualifying
+        teams for the given years. If team is provided, only get tackle
+        for loss data for that team.
+
+        Args:
+            side_of_ball (str): Offense or defense
+            start_year (int): Year to start getting tackle for loss data
+            end_year (int): Year to stop getting tackle for loss data
+            team (str): Team for which to get tackle for loss data
+
+        Returns:
+            Union[TacklesForLoss, list[TacklesForLoss]]: Tackles for
+                loss or opponent tackles for loss for all teams or only
+                for one team
+        """
+        if end_year is None:
+            end_year = start_year
+
+        qualifying_teams = Team.get_qualifying_teams(
+            start_year=start_year, end_year=end_year)
+
+        query = cls.query.join(Team).filter(
+            cls.side_of_ball == side_of_ball,
+            cls.year >= start_year,
+            cls.year <= end_year
+        )
+
+        if team is not None:
+            tfl = query.filter_by(name=team).all()
+            return sum(tfl[1:], tfl[0])
+
+        tfl = {}
+        for team_name in qualifying_teams:
+            team_tfl = query.filter_by(name=team_name).all()
+
+            if team_tfl:
+                tfl[team_name] = sum(team_tfl[1:], team_tfl[0])
+
+        return [tfl[team] for team in sorted(tfl.keys())]
 
     @classmethod
     def add_tackles_for_loss(cls, start_year: int = None,
