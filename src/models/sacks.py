8d73a1235b4e83ca61b0ef086f213a76f46f532a
jtillman20 <jordanmtillman@gmail.com>
2022-03-19 16:53:16 -0400

    Implement methods for adding sack data to database

--- src/models/sacks.py
+++ src/models/sacks.py
@@ -1,4 +1,11 @@
+from operator import attrgetter
+from typing import Union
+
 from app import db
+from scraper import CFBStatsScraper
+from .game import Game
+from .passing import Passing
+from .team import Team
 
 
 class Sacks(db.Model):
@@ -30,6 +37,72 @@ class Sacks(db.Model):
         if self.sacks:
             return self.yards / self.sacks
         return 0.0
+
+    @classmethod
+    def add_sacks(cls, start_year: int = None, end_year: int = None) -> None:
+        """
+        Get sack and opponent sack stats for all teams for the given
+        years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding sack stats
+            end_year (int): Year to stop adding sack stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            years = [year.year for year in query]
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding sack stats for {year}')
+            cls.add_sacks_for_one_year(year=year)
+
+    @classmethod
+    def add_sacks_for_one_year(cls, year: int) -> None:
+        """
+        Get sack and opponent sack stats for all teams for one year and
+        add them to the database.
+
+        Args:
+            year (int): Year to add sack stats
+        """
+        scraper = CFBStatsScraper(year=year)
+
+        for side_of_ball in ['offense', 'defense']:
+            sacks = []
+
+            html_content = scraper.get_html_data(
+                side_of_ball=side_of_ball, category='20')
+            sacks_data = scraper.parse_html_data(
+                html_content=html_content)
+
+            for item in sacks_data:
+                team = Team.query.filter_by(name=item[1]).first()
+                opposite_side_of_ball = 'defense' \
+                    if side_of_ball == 'offense' else 'offense'
+                passing = Passing.get_passing(
+                    side_of_ball=opposite_side_of_ball,
+                    start_year=year,
+                    team=team.name
+                )
+
+                sacks.append(cls(
+                    team_id=team.id,
+                    year=year,
+                    side_of_ball=side_of_ball,
+                    games=item[2],
+                    sacks=item[3],
+                    yards=item[4],
+                    pass_attempts=passing.attempts
+                ))
+
+            for team_sacks in sorted(sacks, key=attrgetter('team_id')):
+                db.session.add(team_sacks)
+
+        db.session.commit()
 
     def __add__(self, other: 'Sacks') -> 'Sacks':
         """
