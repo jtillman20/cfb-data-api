c2dda4d4eb0a0a012bde2838bdb144d73edcc14a
jtillman20 <jordanmtillman@gmail.com>
2022-03-19 17:12:46 -0400

    Add sacks endpoint

--- src/api.py
+++ src/api.py
@@ -32,6 +32,7 @@ api.add_resource(
 api.add_resource(ScoringRoute, f'{API_BASE}/scoring/<string:side_of_ball>')
 api.add_resource(
     ScrimmagePlaysRoute, f'{API_BASE}/scrimmage_plays/<string:side_of_ball>')
+api.add_resource(SacksRoute, f'{API_BASE}/sacks/<string:side_of_ball>')
 api.add_resource(
     TacklesForLossRoute, f'{API_BASE}/tackles_for_loss/<string:side_of_ball>')
 api.add_resource(
