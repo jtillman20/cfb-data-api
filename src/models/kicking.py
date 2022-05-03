f8fc4ca1a0388cb5ddeff32a51f4b2e236639b1c
jtillman20 <jordanmtillman@gmail.com>
2022-05-02 20:27:47 -0400

    Implement methods to add field goal data to database

--- src/models/kicking.py
+++ src/models/kicking.py
@@ -1,4 +1,9 @@
+from operator import attrgetter
+
 from app import db
+from scraper import CFBStatsScraper
+from .game import Game
+from .team import Team
 
 
 class FieldGoals(db.Model):
@@ -28,6 +33,66 @@ class FieldGoals(db.Model):
         if self.attempts:
             return self.field_goals / self.attempts * 100
         return 0.0
+
+    @classmethod
+    def add_field_goals(cls, start_year: int = None,
+                        end_year: int = None) -> None:
+        """
+        Get field goal and opponent field goal stats for all teams for
+        the given years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding field goal stats
+            end_year (int): Year to stop adding field goal stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            years = [year.year for year in query]
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding field goal stats for {year}')
+            cls.add_field_goals_for_one_year(year=year)
+
+    @classmethod
+    def add_field_goals_for_one_year(cls, year: int) -> None:
+        """
+        Get field goal and opponent field goal stats for all teams for
+        one year and add them to the database.
+
+        Args:
+            year (int): Year to add field goal stats
+        """
+        scraper = CFBStatsScraper(year=year)
+
+        for side_of_ball in ['offense', 'defense']:
+            field_goals = []
+
+            html_content = scraper.get_html_data(
+                side_of_ball=side_of_ball, category='07')
+            field_goal_data = scraper.parse_html_data(
+                html_content=html_content)
+
+            for item in field_goal_data:
+                team = Team.query.filter_by(name=item[1]).first()
+
+                field_goals.append(cls(
+                    team_id=team.id,
+                    year=year,
+                    side_of_ball=side_of_ball,
+                    games=item[2],
+                    attempts=item[3],
+                    field_goals=item[4]
+                ))
+
+            for team_field_goals in sorted(
+                    field_goals, key=attrgetter('team_id')):
+                db.session.add(team_field_goals)
+
+        db.session.commit()
 
     def __add__(self, other: 'FieldGoals') -> 'FieldGoals':
         """
