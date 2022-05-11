981db025d0246dc687a603c21c6f28baf74dc83e
jtillman20 <jordanmtillman@gmail.com>
2022-05-11 19:59:50 -0400

    Implement data for getting pat data

--- src/models/kicking.py
+++ src/models/kicking.py
@@ -206,6 +206,48 @@ class PATs(db.Model):
         return 0.0
 
     @classmethod
+    def get_pats(cls, side_of_ball: str, start_year: int, end_year: int = None,
+                 team: str = None) -> Union['PATs', list['PATs']]:
+        """
+        Get PATs or opponent PATs for qualifying teams for the given
+        years. If team is provided, only get PAT data for that team.
+
+        Args:
+            side_of_ball (str): Offense or defense
+            start_year (int): Year to start getting PAT data
+            end_year (int): Year to stop getting PAT data
+            team (str): Team for which to get PAT data
+
+        Returns:
+            Union[PATs, list[PATs]]: PATs or opponent PATs for all
+                teams or only for one team
+        """
+        if end_year is None:
+            end_year = start_year
+
+        qualifying_teams = Team.get_qualifying_teams(
+            start_year=start_year, end_year=end_year)
+
+        query = cls.query.join(Team).filter(
+            cls.side_of_ball == side_of_ball,
+            cls.year >= start_year,
+            cls.year <= end_year
+        )
+
+        if team is not None:
+            pats = query.filter_by(name=team).all()
+            return sum(pats[1:], pats[0])
+
+        pats = {}
+        for team_name in qualifying_teams:
+            team_pats = query.filter_by(name=team_name).all()
+
+            if team_pats:
+                pats[team_name] = sum(team_pats[1:], team_pats[0])
+
+        return [pats[team] for team in sorted(pats.keys())]
+
+    @classmethod
     def add_pats(cls, start_year: int = None, end_year: int = None) -> None:
         """
         Get PAT and opponent PAT stats for all teams for the given
