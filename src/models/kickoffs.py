819e9f413b9cd1b4e4e4d02b447e1e6c3af28db4
jtillman20 <jordanmtillman@gmail.com>
2022-05-18 23:20:50 -0400

    Implement methods for adding kickoff data to database

--- src/models/kickoffs.py
+++ src/models/kickoffs.py
@@ -1,4 +1,9 @@
+from operator import attrgetter
+
 from app import db
+from scraper import CFBStatsScraper
+from .game import Game
+from .team import Team
 
 
 class Kickoffs(db.Model):
@@ -37,6 +42,66 @@ class Kickoffs(db.Model):
         if self.kickoffs:
             return self.onside / self.kickoffs * 100
         return 0.0
+
+    @classmethod
+    def add_kickoffs(cls, start_year: int = None, end_year: int = None) -> None:
+        """
+        Get kickoff and opponent kickoff stats for all teams for the
+        given years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding kickoff stats
+            end_year (int): Year to stop adding kickoff stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            years = [year.year for year in query]
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding kickoff stats for {year}')
+            cls.add_kickoffs_for_one_year(year=year)
+
+    @classmethod
+    def add_kickoffs_for_one_year(cls, year: int) -> None:
+        """
+        Get kickoff and opponent kickoff stats for all teams for
+        one year and add them to the database.
+
+        Args:
+            year (int): Year to add kickoff stats
+        """
+        scraper = CFBStatsScraper(year=year)
+
+        for side_of_ball in ['offense', 'defense']:
+            kickoffs = []
+
+            html_content = scraper.get_html_data(
+                side_of_ball=side_of_ball, category='29')
+            kickoff_data = scraper.parse_html_data(html_content=html_content)
+
+            for item in kickoff_data:
+                team = Team.query.filter_by(name=item[1]).first()
+
+                kickoffs.append(cls(
+                    team_id=team.id,
+                    year=year,
+                    side_of_ball=side_of_ball,
+                    games=item[2],
+                    kickoffs=item[3],
+                    yards=item[4],
+                    touchbacks=item[6],
+                    out_of_bounds=item[8],
+                    onside=item[9]
+                ))
+
+            for team_kickoffs in sorted(kickoffs, key=attrgetter('team_id')):
+                db.session.add(team_kickoffs)
+
+        db.session.commit()
 
     def __add__(self, other: 'Kickoffs') -> 'Kickoffs':
         """
