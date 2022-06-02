ead1b2a838bedeedc827e228aa144f299ab6a494
jtillman20 <jordanmtillman@gmail.com>
2022-06-02 19:53:40 -0400

    Implement methods for adding interception data to database

--- src/models/interceptions.py
+++ src/models/interceptions.py
@@ -1,4 +1,9 @@
+from operator import attrgetter
+
 from app import db
+from scraper import CFBStatsScraper
+from .game import Game
+from .team import Team
 
 
 class Interceptions(db.Model):
@@ -28,6 +33,65 @@ class Interceptions(db.Model):
         if self.ints:
             return self.tds / self.ints * 100
         return 0.0
+
+    @classmethod
+    def add_interceptions(cls, start_year: int = None,
+                          end_year: int = None) -> None:
+        """
+        Get interceptions and opponent interceptionss for all teams for
+        the given years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding interception stats
+            end_year (int): Year to stop adding interception stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            end_year = max([year.year for year in query])
+            years = range(2010, end_year + 1)
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding interception stats for {year}')
+            cls.add_interceptions_for_one_year(year=year)
+
+    @classmethod
+    def add_interceptions_for_one_year(cls, year: int) -> None:
+        """
+        Get interceptions and opponent interceptions for all teams for
+        one year and add them to the database.
+
+        Args:
+            year (int): Year to add interception stats
+        """
+        scraper = CFBStatsScraper(year=year)
+        interceptions = []
+
+        html_content = scraper.get_html_data(
+            side_of_ball='offense', category='16')
+        interception_data = scraper.parse_html_data(
+            html_content=html_content)
+
+        for item in interception_data:
+            team = Team.query.filter_by(name=item[1]).first()
+
+            interceptions.append(cls(
+                team_id=team.id,
+                year=year,
+                games=item[2],
+                ints=item[3],
+                yards=item[4],
+                tds=item[5]
+            ))
+
+        for team_interceptions in sorted(
+                interceptions, key=attrgetter('team_id')):
+            db.session.add(team_interceptions)
+
+        db.session.commit()
 
     def __add__(self, other: 'Interceptions') -> 'Interceptions':
         """
