75600ebea800cbfd1abb8adff50becce00f44be7
jtillman20 <jordanmtillman@gmail.com>
2022-06-02 20:49:24 -0400

    Add database relationship for interceptions

--- src/models/team.py
+++ src/models/team.py
@@ -15,6 +15,7 @@ class Team(db.Model):
     field_goals = db.relationship('FieldGoals', backref='team', lazy=True)
     first_downs = db.relationship('FirstDowns', backref='team', lazy=True)
     fourth_downs = db.relationship('FourthDowns', backref='team', lazy=True)
+    interceptions = db.relationship('Interceptions', backref='team', lazy=True)
     kickoff_return_plays = db.relationship(
         'KickoffReturnPlays', backref='team', lazy=True)
     kickoff_returns = db.relationship(
