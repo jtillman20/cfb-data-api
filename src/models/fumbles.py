123b18b23949e9ccd93ee8346b493e54dd14962d
jtillman20 <jordanmtillman@gmail.com>
2022-07-28 21:39:27 -0400

    Implement methods for adding fumble data to database

--- src/models/fumbles.py
+++ src/models/fumbles.py
@@ -1,4 +1,7 @@
 from app import db
+from scraper import CFBStatsScraper
+from .game import Game
+from .team import Team
 
 
 class Fumbles(db.Model):
@@ -57,6 +60,80 @@ class Fumbles(db.Model):
         if self.fumbles:
             return self.fumbles_forced / self.opponent_fumbles * 100
         return 0.0
+
+    @classmethod
+    def add_fumbles(cls, start_year: int = None, end_year: int = None) -> None:
+        """
+        Get fumbles and opponent fumbles for all teams for the given
+        years and add them to the database.
+
+        Args:
+            start_year (int): Year to start adding fumble stats
+            end_year (int): Year to stop adding fumble stats
+        """
+        if start_year is None:
+            query = Game.query.with_entities(Game.year).distinct()
+            end_year = max([year.year for year in query])
+            years = range(2010, end_year + 1)
+        else:
+            if end_year is None:
+                end_year = start_year
+            years = range(start_year, end_year + 1)
+
+        for year in years:
+            print(f'Adding fumble stats for {year}')
+            cls.add_fumbles_for_one_year(year=year)
+
+    @classmethod
+    def add_fumbles_for_one_year(cls, year: int) -> None:
+        """
+        Get fumbles and opponent fumbles for all teams for one year and
+        add them to the database.
+
+        Args:
+            year (int): Year to add fumble stats
+        """
+        scraper = CFBStatsScraper(year=year)
+        fumbles = {
+            team.name: cls(
+                team_id=team.id,
+                year=year,
+                games=0,
+                fumbles=0,
+                fumbles_lost=0,
+                opponent_fumbles=0,
+                fumbles_recovered=0,
+                fumbles_forced=0
+            )
+            for team in Team.get_teams(year=year)
+        }
+
+        for category in ['17', '18', '22']:
+            side_of_ball = 'defense' if category == '18' else 'offense'
+
+            html_content = scraper.get_html_data(
+                side_of_ball=side_of_ball, category=category)
+            fumble_data = scraper.parse_html_data(html_content=html_content)
+
+            for item in fumble_data:
+                team = item[1]
+
+                if category == '17':
+                    fumbles[team].games = item[2]
+                    fumbles[team].fumbles = item[3]
+                    fumbles[team].fumbles_lost = item[4]
+
+                elif category == '18':
+                    fumbles[team].opponent_fumbles = item[3]
+                    fumbles[team].fumbles_recovered = item[4]
+
+                elif category == '22':
+                    fumbles[team].fumbles_forced = item[3]
+
+        for team_fumbles in fumbles.values():
+            db.session.add(team_fumbles)
+
+        db.session.commit()
 
     def __add__(self, other: 'Fumbles') -> 'Fumbles':
         """
